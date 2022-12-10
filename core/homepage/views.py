2f650a9bbf7f53e09048647c272ba1a3a5934c1f
zenonburgos <znburgos@gmail.com>
2022-12-10 09:01:42 -0600

    Petición de login removida para entrar a index.html

--- core/homepage/views.py
+++ core/homepage/views.py
@@ -16,15 +16,17 @@ class MyCount(object):
         self.v2 = self.v1 + 3
         return ''
 
-@login_required
+# @login_required
 def IndexView(request):
     products = Product.objects.all().filter(store=True).order_by('-created_at')
     offers = Product.objects.all().filter(store=True, special_offers=True).order_by('-created_at')
     featured = Product.objects.all().filter(store=True, featured_products=True).order_by('-created_at')
     bestseller = Product.objects.all().filter(store=True, best_seller=True).order_by('-created_at')
     offersCount = Product.objects.filter(store=True, special_offers=True).count()    
-    cart_items = Cart.objects.filter(user=request.user)
-
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
+    else:
+        cart_items = []
     # print(products)
     # print(math.ceil(offersCount/3))      
     context = {
